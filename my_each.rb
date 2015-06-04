9103fd7ced64c179f064634719e10e34f71de194
apradhn <abhijit.pradhan6@gmail.com>
2015-06-04 15:11:51 -0400

    all done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  size = array.size
+  i = 0
+  while i < size
+    i += 1
+    yield(array[i-1])
+  end
+  array
 end
